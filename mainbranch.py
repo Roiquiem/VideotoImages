624fa0fb22c21413ab0f9986279d5614c190005b
Roiquiem <104369535+Roiquiem@users.noreply.github.com>
2022-11-07 11:49:43 +0200

    Update mainbranch.py

--- mainbranch.py
+++ mainbranch.py
@@ -1,6 +1,19 @@
 
 #import relevant libraries
 import cv2 as cv
-import os
 
-#define the necessary folders, video
+def extractFrames(videoPath, desiredPath, intervalInSeconds):
+    count = 0
+    cap = cv2.VideoCapture(videoPath)
+    success, frame = cap.read()
+    success = True
+    while success:
+        cap.set(cv2.CAP_PROP_POS_MSEC, count*1000*intervalInSeconds)
+        success, frame = cap.read()
+        print("read a new frame: ", success)
+        cv2.imwrite(desiredPath + "\\frame%d.jpg" % count, frame)
+        count = count + 1
+        
+# videoPath = the path of the video
+# desiredPath = the path of the directory where you want your images to be saved in
+# intervalInSeconds = the time interval between saved frames, in seconds  
